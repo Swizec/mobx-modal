dcb5c64f1277ccd4848b13abda4bb55461c906d3
Swizec Teller <swizec@swizec.com>
2016-10-27 23:15:05 -0700

    Add decorators and mobx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Store {
+    @observable value = 1;
+}
+
+@observer
 class App extends Component {
+    store = new Store();
+
+    componentDidMount() {
+        window.store = this.store;
+    }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +26,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+            {this.store.value}
         </p>
       </div>
     );
